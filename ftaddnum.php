98a08168f5b463acdfc83bd2473ff9d983d1e9af
K <keenesthan@yahoo.fr>
2013-05-04 14:35:19 +0300

    Update ftaddnum.php

--- ftaddnum.php
+++ ftaddnum.php
@@ -1,5 +1,10 @@
 <!DOCTYPE>
-<head>
+<title>An XHTML 1.0 Strict  template</title>  
+<meta http-equiv="content-type"     content="text/html;charset=utf-8" />
+</head><body>     <p>… do you want explore Your HTML content here …</p>
+<p style="background-color:#454545;color:#fff;">hello world<a href="removeindex.html"><h1>experimentation musicien</h1></a></p>
+<div width="324px">c'est ici div ! Hi</div>
+
 
 
 <?php
