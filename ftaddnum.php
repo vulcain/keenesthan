62a9bf5ea82a7213f11c1d95c26e8215d8aed9b6
K <keenesthan@yahoo.fr>
2013-05-04 14:54:56 +0300

    Create ftaddnum.php

---
+++ ftaddnum.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+<title>An XHTML 1.0 Strict  template</title> 
+<meta http-equiv="content-type"     content="text/html;charset=utf-8" />
+</head>
+<body>     
+<p>… do you want explore Your HTML content here …</p>
+<p style="background-color:#454545;color:#fff;">hello world<a href="removeindex.html">
+<h1>experimentation musicien</h1></a>
+</p>
+<div width="324px">c'est ici div ! Hi</div>
+<?php 
+function add($x,$y) 
+{ $total=$x+$y; return $total; 
+} 
+echo "1 + 16 = " . add(1,16); 
+?> 
+</body>
