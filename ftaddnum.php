385140ac846e37a62127009f306b89e4bdd4fb4d
K <keenesthan@yahoo.fr>
2013-05-05 12:42:02 +0300

    Update ftaddnum.php

--- ftaddnum.php
+++ ftaddnum.php
@@ -7,16 +7,25 @@
 </style>
 </head>
 <body> 
-<div class="high"><img src="http://cache.www.gametracker.com/profile/keenesthan/b_460x42_C000000-222222-333333-cccccc-ffcc00-ffcc00.png"/>expose</div>
+<div class="high"><img src="http://cache.www.gametracker.com/profile/keenesthan/b_460x42_C000000-222222-333333-cccccc-ffcc00-ffcc00.png"/>
+!   expose  your comment here do you thing trvel form</div>
 <p>… do you want explore Your HTML content here …</p>
 <p style="background-color:#454545;color:#fff;">hello world<a href="removeindex.html">
 <h1>experimentation musicien</h1></a>
 </p>
+<form style="border 2px outset #161656;">
+<label>your name : </label><input type="text" id="textform"/>
+<input type="button" id="subtextform" name="your n° pass"/>
+</form>
 <div width="324px">c'est ici div ! Hi</div>
-<?php 
+<script><?php 
 function add($x,$y) 
 { $total=$x+$y; return $total; 
 } 
 echo ("1 + 16 = " . add(1,16).$total); 
-?> 
+?> </script>
+<script>
+document.write("javascript document")
+</script>
+
 </body>
